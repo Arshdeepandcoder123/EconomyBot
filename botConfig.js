48b6a3ad18894d5907bd56188146eb364f9d8320
coder3445 <74102211+coder3445@users.noreply.github.com>
2021-09-23 11:55:13 +0530

    Add files via upload

---
+++ botConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    token: "ODcxOTg2MjEyMjk3MTM4MjE3.YQjScQ.qjcUbM2xYBAjKxX9f9DkXsR6YvU",
+    prefix: "?",
+    admins: [
+        "775717417845522442",
+        "836813018636746772"
+],
+    debug: true,
+    countChannel: "890481408655654952"
+};
